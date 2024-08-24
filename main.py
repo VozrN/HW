fe20623f812cc9406a757c841cca8f6312d434db
VozrN <nosipova84@gmail.com>
2024-08-24 13:34:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# 1st program
+print (9**0.5*5)
+
+# 2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+#3rd program
+a = (2*2+2)
+print(a)
+b = (2*(2+2))
+print(b)
+if a>b:
+    print (a, 'больше ', b)
+else:
+    if a == b:
+        print (a, 'равно ', b)
+    else:
+        print ( b, 'больше ', a)
+
+#4th program
+c = int(123.456*10)
+print(str(c)[-1])
