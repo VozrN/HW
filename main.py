806201e9ab775c711fa627b6c87e4290a01aa386
Nadezhda <nosipova84@gmail.com>
2024-08-24 22:52:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ a = (2*2+2)
 print(a)
 b = (2*(2+2))
 print(b)
-if a>b:
-    print (a, 'больше ', b)
-else:
-    if a == b:
-        print (a, 'равно ', b)
-    else:
-        print ( b, 'больше ', a)
+print(a==b)
+#if a>b:
+   # print (a, 'больше ', b)
+#else:
+    #if a == b:
+      #  print (a, 'равно ', b)
+    #else:
+        #print ( b, 'больше ', a)
 
 #4th program
-c = int(123.456*10)
-print(str(c)[-1])
+k = float(123.456)
+c = int(k*10)
+print(c%10)
