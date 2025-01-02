6609ef4f0cf2afb9448b9bf44777149b026b07bd
Nadezhda <nosipova84@gmail.com>
2025-01-02 18:19:49 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99>9.98 and 1000!=1001)
+
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+#4th program
+stroka='123.456'
+number=float(stroka)
+ten_number=number*10
+four_number=int(ten_number%10)
+print(four_number)
+
+
